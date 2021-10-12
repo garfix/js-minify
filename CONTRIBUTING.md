6f5abfd8d6b064928f61a46e37ea9434669a747b
Patrick van Bergen <patrick.vanbergen@gmail.com>
2021-10-12 21:10:12 +0200

    real-life tests

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,5 +1,25 @@
+# Contributing
+
+When you make a contribution, please check:
+
+- If your code runs using PHP 5.6 (and up)
+- If the tests run
+
 ## Run the tests
 
-To run the unit tests, do
+To run both unit tests and real-life tests on actual libraries, do
 
-    ./vendor/bin/phpunit
+    /usr/bin/php7.3 ./vendor/bin/phpunit
+
+To just run the unit tests, do
+
+    /usr/bin/php7.3 ./vendor/bin/phpunit --group unit
+
+To just run the real-life tests, do
+
+    /usr/bin/php7.3 ./vendor/bin/phpunit --group libraries
+
+The specified version of the PHP runtime is only important for the real-life tests, which test execution time.
+
+If you find that your computer is slower or faster than mine, change the maxDuration values of the real-life tests. 
+Then, after you made your change, check that it hasn't made the code slower.
--- tests/Garfix/JsMinify/Test/MinifierTest.php
+++ tests/Garfix/JsMinify/Test/MinifierTest.php
@@ -7,6 +7,9 @@ use PHPUnit\Framework\TestCase;
 
 class MinifierTest extends TestCase
 {
+    /**
+     * @group unit
+     */
     public function testJsMinify()
     {
         $baseDir = __DIR__ . '/../../../resources/';
@@ -20,6 +23,44 @@ class MinifierTest extends TestCase
             $output = Minifier::minify($testInput);
 
             $this->assertEquals($testOutput, $output, $outputFile);
+        }
+    }
+
+    /**
+     * @group libraries
+     */
+    public function testRealLibraries()
+    {
+        $libraries = [
+            ['https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.js', 28, 0.025],
+            ['https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js', 51, 0.021],
+            ['https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.js', 66.5, 0.16]
+        ];
+
+        echo "\n";
+
+        foreach ($libraries as $library) {
+            list($url, $maxPercentage, $maxDuration) = $library;
+            $js = file_get_contents($url);
+
+            $minifiedCode = Minifier::minify($js);
+            $percentage = (strlen($minifiedCode) / strlen($js)) * 100;
+
+            $duration = 1000;
+            // execution time: best of 5
+            for ($i = 0; $i < 5; $i++) {
+                $start = microtime(true);
+                Minifier::minify($js);
+                $end = microtime(true);
+                $duration = min($duration, $end - $start);
+            }
+
+            echo $url . ": percentage = " . sprintf("%0.2f", $percentage) . "; duration = " . sprintf("%0.3f", $duration) . ")\n";
+
+            $this->assertNotEquals(0, $percentage);
+            $this->assertNotEquals(100, $percentage);
+            $this->assertLessThanOrEqual($maxDuration, $duration);
+            $this->assertLessThanOrEqual($maxPercentage, $percentage);
         }
     }
 }
